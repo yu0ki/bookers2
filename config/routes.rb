78f76d3e0796821c2357977d47b11aaea8e5ed50
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-20 12:30:08 +0000

    [Add]Bookモデルの作成とアソシエーション

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   
+  # users_controller
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # homes_controller
+  root :to  => "homes#top"
+  
+  # books_controller
+  resources :books
+  
+ 
+  
 end
