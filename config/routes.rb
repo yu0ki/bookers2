a920e49ddc5ccdf89f0d2704ceb05bf2715aa615
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-21 04:32:30 +0000

    [Add]ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   # users_controller
   devise_for :users
-  get "users" => "users#index"
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # homes_controller
