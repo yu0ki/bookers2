52c566b1a2aaeeacf02aac02c7f24f27c8b6fb41
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-21 01:51:51 +0000

    [Add]フッターの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   # users_controller
   devise_for :users
+  get "users" => "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # homes_controller
