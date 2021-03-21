8aec37396f88d3b9e104941607db32ba148c79a3
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-21 10:56:32 +0000

    [Add]投稿機能追加！！

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,29 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all
+        @book = Book.new
+    end
+    
+    def new
+        @book = Book.new
+    end
+    
+    def create
+        p params
+        
+        book = Book.new(book_params)
+        book.user_id = current_user.id
+        if book.save
+            redirect_to book_path(book)
+        else
+           render "/books/index" 
+        end
+    end
+    
+    
+    private
+    
+    def book_params
+       params.require(:book).permit(:title, :body) 
+    end
 end
