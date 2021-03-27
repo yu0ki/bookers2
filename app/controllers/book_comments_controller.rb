8e22279d4bd860cac09aaaf0152df53458aa0e9b
yu0ki <i0t0o0y0u0k0i@gmail.com>
2021-03-27 11:49:16 +0000

    [Add]コメント削除機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -17,6 +17,20 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:book_id])
+    book_comment = BookComment.find_by(book_id: book.id)
+    if book_comment.destroy
+      flash[:notice] = "You have deleted comment successfully."
+      redirect_to request.referer
+    else
+      flash[:alert] = "Your comment was not deleted. Please try again."
+      @book = book   
+      @new_book = Book.new
+      @book_comments = BookComment.page(params[:page]).reverse_order
+      @book_comment = book_comment
+      render "books/show"
+    end
+    
   end
   
   
